e84f60769bb4be079e1656db07e783d20273daa4
Mriga195 <mrigank195@gmail.com>
2024-06-14 20:52:06 +0530

    Add hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> sortRev(vector<int>& arr);
+
+int main()
+{
+    vector<int> arr = {31, 41, 59, 26, 41, 58};
+    vector<int> sorted = sortRev(arr);
+
+    for (int num: sorted)
+        cout << num << " ";
+
+    cout << endl;
+}
+
+vector<int> sortRev(vector<int>& arr)
+{
+    int length = arr.size();
+    for (int i = 1; i < length; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && key > arr[j])
+        {
+            arr[j+1] = arr[j];
+            j--;
+        }
+
+        arr[j+1] = key;
+    }
+
+    return arr;
+}
